55a91d4a64efde7945a0e081e866f02751ee23ce
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-08-15 01:47:55 +0100

    Update home.js

--- src/views/home.js
+++ src/views/home.js
@@ -63,8 +63,8 @@ const Home = (props) => {
             <div className="home-mid">
               <div className="home-links-container2">
                 <span className="home-link14 Anchor">features</span>
-                <a href="#services" className="home-link15 Anchor">
-                  services
+                <a href="/presale" className="home-link15 Anchor">
+                  Presale
                 </a>
                 <a href="#about-us" className="home-link16 Anchor">
                   About Us
